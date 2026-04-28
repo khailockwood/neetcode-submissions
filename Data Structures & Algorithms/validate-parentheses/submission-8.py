18ace31530f3c8b97fb89f21da201e541eeb6edc
Khai Lockwood <khailockwood@gmail.com>
2026-04-28 15:58:02 -0400

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        #last in first out (stack), every bracket that goes in needs to come out
+        #order they come in have to be order they come out of stack
+        for char in s:
+            if char == "{" or char == "[" or char == "(" or len(stack) == 0:
+                stack.append(char)
+                print(stack)
+            elif char == "}":
+                if stack.pop() != "{":
+                    return False
+            elif char == "]":
+                if stack.pop() != "[":
+                    return False
+            elif char == ")":
+                if stack.pop() != "(":
+                    return False
+        if len(stack) == 0:
+            return True
+        else:
+            return False
