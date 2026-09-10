60a145539718e4d2078ce33006c4e4d7d5312218
Khai Lockwood <khailockwood@gmail.com>
2026-09-10 19:12:20 -0400

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        maxNum = 0
+        cache = {}
+
+        def findMax(index, cache):
+            if index >= len(nums):
+                return 0
+            if index in cache:
+                return cache[index]
+
+            cache[index] = max(nums[index] + findMax(index + 2, cache), findMax(index + 1, cache))
+
+            return cache[index]
+        
+        res = findMax(0, cache)
+        if res > maxNum:
+            maxNum = res
+
+        return maxNum
